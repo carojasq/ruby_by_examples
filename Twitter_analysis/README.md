0b3391799b8310eae4534c5110a5cb45d51d418f
alejo0317 <alejandro.0317@gmail.com>
2014-10-05 16:55:40 -0500

    Twitter word analysis

---
+++ Twitter_analysis/README.md
@@ -0,0 +1,15 @@
+<h1>Twitter historic word count</h1>
+Counts the most used words for a user in twitter.
+<h2>Instructions</h2>
+<li>
+<ul> Configure your consumer key and your consumer secret on config.rb file.  </ul>
+<ul> Install necessary gems </ul>
+
+  ````bash
+bundle install
+  ````
+<ul> Run th script! </ul>
+  ````bash
+  ruby main.rb username
+  ````
+</li>
--- Twitter_analysis/main.rb
+++ Twitter_analysis/main.rb
@@ -1,4 +1,88 @@
-load 'config.rb'
+require_relative 'config'
+require 'twitter'
+require 'irb/completion'
+require 'set'
+require "i18n"
 
-client = Twitter::REST::Client.new(config.consumer_data)
+# To filter words
+COMMON_WORDS = {
+  :spanish    => ["a", "ante", "bajo", "cabe", "con", "contra", "de", "desde", "durante", "en", "entre", "hacia", "hasta" \
+  		"hacia", "hasta", "mediante", "para", "por", "según", "sin", "so", "sobre", "tras", "versus", "la", "que", "y", \
+  		 "no", "es", "los", "una", "las", "como", "de", "cuando", "mas", "ahora", "qué", "son", "les", "bien", "del", \
+  		 "pero", "the", "esto", "esta"],  
+}
 
+# To obtain twitter data
+client = Twitter::REST::Client.new(CONSUMER_DATA)
+
+# a_user = client.user("alejo0317")
+def client.collect_with_max_id(collection=[], max_id=nil, &block)
+  response = yield(max_id)
+  collection += response
+  puts collection
+  response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
+end
+
+def client.get_all_tweets(user)
+  collect_with_max_id do |max_id|
+    options = {:count => 200, :include_rts => true}
+    options[:max_id] = max_id unless max_id.nil?
+    user_timeline(user, options)
+  end
+end
+if ARGV[0].downcase =~ /^[a-z0-9\_]+$/
+  username = ARGV[0]
+else 
+  puts "#{ARGV[0]} is not a valid username"
+  exit
+end
+
+tweets_dump_f = username + "_tweets.dump"
+
+# Using marshal for tweets persistence
+if File.exist?(tweets_dump_f)
+	puts "Loading tweets from cache for user @#{username}"
+	tweets = nil
+	File.open(tweets_dump_f,"rb") {|f| tweets = Marshal.load(f)}
+else
+  begin
+    puts "Fetching tweets of @#{username}, please wait"
+	  tweets = client.get_all_tweets(username)
+  rescue
+    puts "Can't fetch tweets, check your internet conection or the username inputted"
+    exit
+  end
+	dmp = Marshal.dump(tweets)
+	File.open(tweets_dump_f, 'w') {|f| f.write(dmp) }
+
+end
+# Extracting useful info
+puts "There are #{tweets.count} tweets"
+all_text = ""
+tweets.collect{|t| all_text += t.text}
+words = all_text.downcase.gsub("\n",' ').split(' ')
+# Clean accents
+# words = I18n.transliterate(all_text).downcase.gsub("\n",' ').split(' ')
+tags = words.select { |f| f =~ /^#.*$/ }
+others = words.select { |f| f =~ /^@.*$/ }
+only_urls = words.select{|f| f =~ /^https\/\/.*$/}
+only_words = words.reject{|f| f =~ /^#.*$/ || f =~ /^@.*$/ ||  f.length <= 2  || f =~ /^https\/\/.*$/ } # || PREPOSITIONS[:spanish].include? f
+
+puts "Counting words"
+word_count = { }
+words_set = only_words.to_set - COMMON_WORDS[:spanish].to_set
+words_set.each do |word|
+	word_count[word] = only_words.select{|w| w == word}.count
+end
+
+puts "Sorting"
+sorted = word_count.sort_by { |word, count| count }
+elements_n = sorted.count
+sorted.each do |s| 
+  elements_n -=  1
+  puts elements_n.to_s+". Word: "+s[0]+", count: "+s[1].to_s 
+end
+
+#puts tweet_class.to_s
+#puts tweet_class.methods
+# XXX A pythons ipdb equivalent for debugging in rubyh

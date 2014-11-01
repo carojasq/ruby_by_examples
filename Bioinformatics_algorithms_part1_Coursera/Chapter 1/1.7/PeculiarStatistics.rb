b3985e0fafb269ea15579821806b182488fa21d4
alejo0317 <alejandro.0317@gmail.com>
2014-10-31 23:32:30 -0500

    Peculiar Statistics

---
+++ Bioinformatics_algorithms_part1_Coursera/Chapter 1/1.7/PeculiarStatistics.rb
@@ -0,0 +1,27 @@
+def Skew(in_seq)
+	skews = [0]
+	(0..in_seq.length-1).each do |i|
+		if in_seq[i]=="C"
+			skews << skews.last-1
+		elsif in_seq[i]=="G"
+			skews << skews.last+1
+		else
+			skews << skews.last
+		end
+	end
+	return skews
+end
+
+def MinSkews(in_seq)
+	skews = Skew(in_seq)
+	min = skews.min
+	return skews.each_index.select{|i| skews[i]==min}
+end
+
+#MinSkews
+in_seq = gets.chomp	
+puts MinSkews(in_seq).join(" ")
+
+
+#Skew input
+#puts Skew("GAGCCACCGCGATA").join " "
